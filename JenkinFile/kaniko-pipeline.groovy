023b40c8afba4138af9d4382bd561e0584522735
Hồ Thanh Lộc <132435500+RibunLoc@users.noreply.github.com>
2025-12-30 22:18:33 +0700

    Update kaniko command to use sleep for 365 days

--- JenkinFile/kaniko-pipeline.groovy
+++ JenkinFile/kaniko-pipeline.groovy
@@ -21,6 +21,9 @@ spec:
         tty: true
       - name: kaniko
         image: gcr.io/kaniko-project/executor:v1.23.2
+        command: ["/busybox/sh", "-c"]
+        args: ["sleep 365d"]
+        tty: true
         volumeMounts:
           - name: docker-config
             mountPath: /kaniko/.docker/
