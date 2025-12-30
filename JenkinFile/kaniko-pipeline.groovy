f224b7b45fe74fa55fa7642feb361a6ddeb3270b
Hồ Thanh Lộc <132435500+RibunLoc@users.noreply.github.com>
2025-12-30 21:40:13 +0700

    Add Kaniko pipeline for building and pushing images

---
+++ JenkinFile/kaniko-pipeline.groovy
@@ -0,0 +1,61 @@
+pipeline {
+    agent {
+        kubernetes {
+            defaultContainer 'node'
+            yaml """
+apiVersion: v1
+kind: Pod
+spec:
+    restartPolicy: Never
+    volumes:
+      - name: docker-config
+        secret:
+          secretName: regcred
+          items:
+            - key: .dockerconfigjson
+              path: config.json
+    containers:
+      - name: node
+        image: node:20-bookworm
+        command: ["cat"]
+        tty: true
+      - name: kaniko
+        image: gcr.io/kaniko-project/executor:v1.23.2
+        command: ["cat"]
+        tty: true
+        volumeMounts:
+          - name: docker-config
+            mountPath: /kaniko/.docker/
+            readOnly: true
+"""
+        }
+    }
+
+    environment {
+        IMAGE_REPO = "ribun/my-app-kaniko"
+        CACHE_REPO = "ribun/kaniko-cache"
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+
+        stage('Build and Push Image') {
+            steps {
+                container('kaniko') {
+                    sh '''
+                      /kaniko/executor \
+                        --context=dir://$WORKSPACE \
+                        --dockerfile=$WORKSPACE/Dockerfile \
+                        --destination=$IMAGE_REPO:latest \
+                        --cache=true \
+                        --cache-repo=$CACHE_REPO
+                    '''
+                }
+            }
+        }
+    }
+}
