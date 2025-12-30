21fabe98c33fd698fdcbe5f3234f091eee7caccb
Hồ Thanh Lộc <132435500+RibunLoc@users.noreply.github.com>
2025-12-30 22:15:09 +0700

    Remove redundant command and tty for kaniko

--- JenkinFile/kaniko-pipeline.groovy
+++ JenkinFile/kaniko-pipeline.groovy
@@ -21,8 +21,6 @@ spec:
         tty: true
       - name: kaniko
         image: gcr.io/kaniko-project/executor:v1.23.2
-        command: ["cat"]
-        tty: true
         volumeMounts:
           - name: docker-config
             mountPath: /kaniko/.docker/
